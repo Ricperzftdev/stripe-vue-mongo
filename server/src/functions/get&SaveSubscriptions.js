de942ed823fefc1a1d57ee2f83bbfe91335de764
Ricperz <richardfrontend@gmail.com>
2023-08-29 14:32:02 -0400

    sorting products from stripe

--- server/src/functions/get&SaveSubscriptions.js
+++ server/src/functions/get&SaveSubscriptions.js
@@ -5,5 +5,17 @@ const stripe = new Stripe(STRIPE_API_KEY);
 
 (async () => {
     const { data } = await stripe.prices.list();
-    console.log(data)
+    const products = [];
+
+    for (const element of data) {
+        const product = {
+            id: element.id,
+            name: element.nickname,
+            currency: element.currency,
+            price: element.unit_amount / 100
+        };
+        products.push(product);
+    };
+    const productsSorted = products.sort((a, b) => a.price - b.price);
+    console.log(productsSorted)
 })();
